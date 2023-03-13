187bf066ddb2eb7ad26472f63d7f61fbb3bb5805
randerson2720 <ryananderson2727@gmail.com>
2023-03-12 22:00:13 -0500

    added contact code

--- forms/contact.php
+++ forms/contact.php
@@ -1,24 +1,17 @@
 <?php
-  $receiving_email_address = 'ryananderson2727@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['subject']) && isset($_POST['message'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+    $to = "ryananderson@gmail.com";
+    $body = "Name: $name\nEmail: $email\n\nSubject: $subject\n\nMessage:\n$message";
 
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
-?>
+    if (mail($to, $subject, $body)) {
+        echo "sent";
+    } else {
+        echo "failed";
+    }
+}
